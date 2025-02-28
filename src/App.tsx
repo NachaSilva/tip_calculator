151188f12a6ee01b1d186218fe943db336b522f8
Ignacia Silva <ignaciasilva1@gmail.com>
2025-02-28 09:13:53 -0300

    create tip and calculations of total products and products plus tips

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { menuItems } from "./data/db";
 import  MenuItem  from "./components/MenuItem"
 import useOrder from "./hooks/useOrder";
 import OrderContents from "./components/OrderContents";
+import OrderTotals from "./components/OrderTotals";
+import TipPercentageForm from "./components/TipPercentageForm";
 
 function App() {
-  const {order, addItem, removeItem} = useOrder()
+  const {order, addItem, removeItem, tip, setTip} = useOrder()
 
   return (
     <>
@@ -37,7 +39,15 @@ function App() {
          order= {order}
          removeItem = {removeItem}
          />
+         <TipPercentageForm
+         setTip = {setTip}
+         />
+              <OrderTotals
+        order = {order}
+        tip ={tip}
+        />
         </div>
+   
       </main>
     </>
   );
