60f9021943d27c92cfd964ebe710db35be7fccfc
Ignacia Silva <ignaciasilva1@gmail.com>
2025-03-03 00:11:22 -0300

    replace useState to useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { menuItems } from "./data/db";
-import  MenuItem  from "./components/MenuItem"
-import useOrder from "./hooks/useOrder";
+import MenuItem from "./components/MenuItem";
 import OrderContents from "./components/OrderContents";
 import OrderTotals from "./components/OrderTotals";
 import TipPercentageForm from "./components/TipPercentageForm";
+import { useReducer } from "react";
+import { initialState, orderReducer } from "./reducers/order-reducer";
 
 function App() {
-  const {order, addItem, removeItem, tip, setTip, placeOrder} = useOrder()
+
+
+  const [state, dispatch] = useReducer(orderReducer, initialState);
 
   return (
     <>
@@ -16,48 +19,27 @@ function App() {
         </h1>
       </header>
 
-     
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
         <div className="p-5">
           <h2 className="text-4xl font-black">Menú</h2>
 
           <div className="space-y-3 mt-10">
-          {menuItems.map(item =>(
-            <MenuItem
-            key={item.id}
-            item={item}
-            addItem ={addItem}
-            />
-          )
-
-          )}
-        </div>
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} dispatch={dispatch} />
+            ))}
+          </div>
         </div>
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-         {order.length > 0 ? (
-          <>
-           <OrderContents
-
-order= {order}
-removeItem = {removeItem}
-/>
-<TipPercentageForm
-setTip = {setTip}
-tip={tip}
-/>
-     <OrderTotals
-order = {order}
-tip ={tip}
-placeOrder = {placeOrder}
-/>
-          </>
-         ):(
-          <p className="text-center"> La orden está vacía</p>
-         )}
-         
-        
+          {state.order.length > 0 ? (
+            <>
+              <OrderContents order={state.order} dispatch={dispatch}/>
+              <TipPercentageForm dispatch = {dispatch} tip={state.tip} />
+              <OrderTotals order={state.order} tip={state.tip} dispatch={dispatch} />
+            </>
+          ) : (
+            <p className="text-center"> La orden está vacía</p>
+          )}
         </div>
-   
       </main>
     </>
   );
